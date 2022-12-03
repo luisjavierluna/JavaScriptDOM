b559e052afecf441015cfbb0d66fec30c65baa32
luisjavierluna <javierlunaluis@gmail.com>
2022-12-03 13:48:33 -0600

    145. Seleccionar Elementos con querySelectorAll

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ const heading = document.querySelector('.header__texto h2') // Retorna 0 o 1 ele
 heading.textContent = 'Nuevo Heading'
 // heading.classList.add('nueva-clase')
 console.log(heading)
+
+// querySelectorAll
+const enlaces = document.querySelectorAll('.navegacion a')
+enlaces[0].textContent = 'Nuevo Texto para Enlace'
+enlaces[0].classList.add('nueva-clase')
+enlaces[0].classList.remove('navegacion__enlace')
+
+// getElementById
+
+const heading2 =  document.getElementById('heading')
+console.log(heading2)
