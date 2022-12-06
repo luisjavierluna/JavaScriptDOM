8124475f3a1bbda40f003b29d1184a4497dc71e5
luisjavierluna <javierlunaluis@gmail.com>
2022-12-05 22:22:14 -0600

    146. Crear HTML con createElement

--- js/script.js
+++ js/script.js
@@ -8,11 +8,30 @@ console.log(heading)
 
 // querySelectorAll
 const enlaces = document.querySelectorAll('.navegacion a')
-enlaces[0].textContent = 'Nuevo Texto para Enlace'
-enlaces[0].classList.add('nueva-clase')
-enlaces[0].classList.remove('navegacion__enlace')
+// enlaces[0].textContent = 'Nuevo Texto para Enlace'
+// enlaces[0].classList.add('nueva-clase')
+// enlaces[0].classList.remove('navegacion__enlace')
 
 // getElementById
 
 const heading2 =  document.getElementById('heading')
 console.log(heading2)
+
+// Generar un nuevo enlace
+const nuevoEnlace = document.createElement('A')
+
+// Agregar el Href
+nuevoEnlace.href = 'nuevo-enlace.html'
+
+// Agregar el texto
+nuevoEnlace.textContent = 'Un nuevo enlace'
+
+// Agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace')
+
+// Agregarlo al documeto
+const navegacion = document.querySelector('.navegacion')
+
+navegacion.appendChild(nuevoEnlace)
+
+console.log(nuevoEnlace)
