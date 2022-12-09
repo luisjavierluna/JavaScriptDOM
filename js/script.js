f0bcb9ccd237dd60dd99ec48edc133b793254cb2
luisjavierluna <javierlunaluis@gmail.com>
2022-12-09 17:16:53 -0600

    148. Reaccionar a Clicks en JavaScript

--- js/script.js
+++ js/script.js
@@ -60,3 +60,14 @@ function imprimir() {
 window.onscroll = function() {
     console.log('scrolling...')
 }
+
+// Seleccionar elementos y asociarles un evento
+const btnEnviar = document.querySelector('.boton--primario')
+btnEnviar.addEventListener('click', function(evento) {
+    console.log(evento)
+    evento.preventDefault()
+
+    // Validar un formulario
+
+    console.log('enviando formulario')
+}) 
